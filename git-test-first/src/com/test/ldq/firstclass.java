3dfd0bdb623a1e3a5634f18936c1d46928b4cab9
a840590148 <840590148@qq.com>
2020-03-23 16:35:01 +0800

    test

---
+++ git-test-first/src/com/test/ldq/firstclass.java
@@ -0,0 +1,12 @@
+package com.test.ldq;
+
+public class firstclass {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
+}
